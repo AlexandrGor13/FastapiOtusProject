e1241c09e09b095776b40b68ef3b300d8d2d8b9e
alexandr <gorbenich@gmail.com>
2025-11-24 22:55:26 +0300

    refactoring

--- app/api/api/image.py
+++ app/api/api/image.py
@@ -1,27 +1,30 @@
-from typing import List, Annotated
+from typing import Annotated
 from fastapi import APIRouter, File, UploadFile, status, Body
 import io
 from PIL import Image
 import numpy as np
+from starlette.responses import JSONResponse
+
 from deepface import DeepFace
+from deepface.modules.detection import extract_faces
 
 router = APIRouter(tags=["DeepFace"], prefix="/image")
 
 
 @router.post(
     "/recognize-face/",
-    status_code = status.HTTP_200_OK,
-    summary = "Recognize Face",
-    responses = {
+    status_code=status.HTTP_200_OK,
+    summary="Recognize Face",
+    responses={
         status.HTTP_200_OK: {
             "description": "Recognize Face",
             "content": {
                 "application/json": {
                     "example": {
-                          "result": "Возраст: 35, Пол: мужчина, Эмоция: нейтральная",
-                          "age": 35,
-                          "gender": "Man",
-                          "emotion": "neutral"
+                        "result": "Возраст: 35, Пол: мужчина, Эмоция: нейтральная",
+                        "age": 35,
+                        "gender": "Man",
+                        "emotion": "neutral"
                     }
                 }
             }
@@ -40,7 +43,8 @@ async def recognize_face(file: UploadFile = File(...)):
     image_array = np.array(img)
 
     try:
-        result = DeepFace.analyze(image_array, actions=('age', 'gender', 'emotion'))
+        result = DeepFace.analyze(image_array, actions=('age', 'gender', 'emotion'), detector_backend='yolov8n')
+        print(len(result))
         if len(result) != 1:
             raise ValueError('На изображении более одного лица')
         img_age = result[0].get('age')
@@ -68,7 +72,10 @@ async def recognize_face(file: UploadFile = File(...)):
             "emotion": result[0].get('dominant_emotion'),
         }
     except Exception as e:
-        return {"error": str(e)}
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content={"detail": str(e)},
+        )
 
 
 @router.post(
@@ -92,9 +99,10 @@ async def recognize_face(file: UploadFile = File(...)):
 async def compare_faces(
         file1: UploadFile = File(...),
         file2: UploadFile = File(...),
-        model_name: Annotated[str, Body(...,
-            description="Model for face recognition. Options: VGG-Face, Facenet, Facenet512, DeepFace, ArcFace"
-        )] = "VGG-Face"
+        model_name: Annotated[str,
+        Body(...,
+             description="Model for face recognition. Options: VGG-Face, Facenet, Facenet512, DeepFace, ArcFace"
+             )] = "VGG-Face"
 ):
     """
     Сравнивает два загруженных изображения на предмет схожести лиц.
@@ -114,4 +122,44 @@ async def compare_faces(
             "distance": result.get("distance"),
         }
     except Exception as e:
-        return {"error": str(e)}
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content={"detail": "Server Error"},
+        )
+
+
+@router.post(
+    "/count-people/",
+    status_code=status.HTTP_200_OK,
+    summary="Count people",
+    responses={
+        status.HTTP_200_OK: {
+            "description": "Count people",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "count people": 2
+                    }
+                }
+            }
+        }
+    }
+)
+async def count_people(file: UploadFile = File(...)):
+    """
+    Сравнивает два загруженных изображения на предмет схожести лиц.
+    """
+
+    contents = await file.read()
+    img = Image.open(io.BytesIO(contents))
+    image_array = np.array(img)
+    try:
+        result = extract_faces(image_array, detector_backend='yolov8n')
+        return {
+            "count people": len(result),
+        }
+    except Exception as e:
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content={"detail": "Server Error"},
+        )
