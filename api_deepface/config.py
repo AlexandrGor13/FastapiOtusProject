7bc6b6659052b1e42d49e4e548d62773130e7ba9
alexandr <gorbenich@gmail.com>
2025-11-28 23:55:21 +0300

    update deepface

--- api_deepface/config.py
+++ api_deepface/config.py
@@ -2,8 +2,8 @@ import logging
 
 logging.basicConfig(
     level=logging.INFO,
-    format='[%(asctime)s] %(levelname).1s %(message)s',
-    datefmt='%Y.%m.%d %H:%M:%S'
+    format="%(levelprefix)s | %(asctime)s | %(module)s:%(lineno)d | %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
 )
 
 log = logging.getLogger(__name__)
