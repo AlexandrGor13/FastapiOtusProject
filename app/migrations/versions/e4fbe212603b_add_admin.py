d35bdb4f94e8a7abd419efc6406928f7f374eb62
alexandr <gorbenich@gmail.com>
2025-12-09 00:05:04 +0300

    refactoring

--- app/migrations/versions/e4fbe212603b_add_admin.py
+++ app/migrations/versions/e4fbe212603b_add_admin.py
@@ -27,6 +27,10 @@ def upgrade() -> None:
         f"""
         INSERT INTO users (username, email, password_hash, role)
         VALUES ('{settings.admin.user}', 'admin@admin.admins', '{get_password_hash(settings.admin.password)}', 'admins');
+        """
+    )
+    op.execute(
+        """
         INSERT INTO profiles (first_name, last_name, phone, user_id)
         VALUES ('', '', '+71234445566', '1');
         """
@@ -35,6 +39,5 @@ def upgrade() -> None:
 
 def downgrade() -> None:
     # Удаляем нового пользователя
-    op.execute(
-        "DELETE FROM profiles WHERE user_id = '1'; DELETE FROM users WHERE username = 'admin';"
-    )
+    op.execute("DELETE FROM profiles WHERE user_id = '1';")
+    op.execute("DELETE FROM users WHERE username = 'admin';")
